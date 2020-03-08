a45edc99fd80134f9c607388bb65d9f62d60c085
sagar <sagarkdalavi@gmail.com>
2020-03-08 14:07:03 +0530

    Added useState hook

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,33 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from '../UI/Card'
+import './IngredientForm.css'
 
 const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
-    event.preventDefault();
-    // ...
-  };
+    const [inputState, setInputState] = useState({ title: '', amount: '' })
+    const submitHandler = event => {
+        event.preventDefault()
+        // ...
+    }
 
-  return (
-    <section className="ingredient-form">
-      <Card>
-        <form onSubmit={submitHandler}>
-          <div className="form-control">
-            <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
-          </div>
-          <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
-          </div>
-          <div className="ingredient-form__actions">
-            <button type="submit">Add Ingredient</button>
-          </div>
-        </form>
-      </Card>
-    </section>
-  );
-});
+    return (
+        <section className="ingredient-form">
+            <Card>
+                <form onSubmit={submitHandler}>
+                    <div className="form-control">
+                        <label htmlFor="title">Name</label>
+                        <input
+                            type="text"
+                            id="title"
+                            value={inputState.title}
+                            onChange={event => {
+                                const newTitle = event.target.value
+                                setInputState(preState => ({
+                                    title: newTitle,
+                                    amount: preState.amount,
+                                }))
+                            }}
+                        />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="amount">Amount</label>
+                        <input
+                            type="number"
+                            id="amount"
+                            value={inputState.amount}
+                            onChange={event => {
+                                const newAmount = event.target.value
+                                setInputState(preState => ({
+                                    title: preState.title,
+                                    amount: newAmount,
+                                }))
+                            }}
+                        />
+                    </div>
+                    <div className="ingredient-form__actions">
+                        <button type="submit">Add Ingredient</button>
+                    </div>
+                </form>
+            </Card>
+        </section>
+    )
+})
 
-export default IngredientForm;
+export default IngredientForm
