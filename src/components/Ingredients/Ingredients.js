26fd6f201aedcbf595b0f3269156cf32d0c9565b
sagar <sagarkdalavi@gmail.com>
2020-03-08 22:51:20 +0530

    Passing state across components

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from './IngredientForm'
+import IngredientList from './IngredientList'
+import Search from './Search'
 
-function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
+const Ingredients = () => {
+    const [userIngredients, setUserIngredients] = useState([])
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
-  );
+    const addIngredientHandler = ingredient => {
+        setUserIngredients(preState => [
+            ...preState,
+            { id: Math.random().toString(), ...ingredient },
+        ])
+    }
+
+    const removeIngredientHandler = id => {
+        setUserIngredients(preState =>
+            preState.filter(ingredient => ingredient.id !== id)
+        )
+    }
+
+    return (
+        <div className="App">
+            <IngredientForm onAddIngredient={addIngredientHandler} />
+
+            <section>
+                <Search />
+                <IngredientList
+                    ingredients={userIngredients}
+                    onRemoveItem={id => {
+                        removeIngredientHandler(id)
+                    }}
+                />
+            </section>
+        </div>
+    )
 }
 
-export default Ingredients;
+export default Ingredients
