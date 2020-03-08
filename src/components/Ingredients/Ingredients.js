3d45e25e9153c0e194c5a0ebb5cff88bb579084c
sagar <sagarkdalavi@gmail.com>
2020-03-08 23:19:00 +0530

    Added useEffect and load data from HTTP Get

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import IngredientForm from './IngredientForm'
 import IngredientList from './IngredientList'
@@ -6,6 +6,23 @@ import Search from './Search'
 
 const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([])
+
+    useEffect(() => {
+        fetch('https://react-hooks-d3fc1.firebaseio.com/ingredients.json')
+            .then(response => response.json())
+            .then(responseData => {
+                console.log('response data:', responseData)
+                const loadingIngredients = []
+                for (const key in responseData) {
+                    loadingIngredients.push({
+                        id: key,
+                        title: responseData[key].title,
+                        amount: responseData[key].amount,
+                    })
+                }
+                setUserIngredients(loadingIngredients)
+            })
+    }, [])
 
     const addIngredientHandler = ingredient => {
         fetch('https://react-hooks-d3fc1.firebaseio.com/ingredients.json', {
