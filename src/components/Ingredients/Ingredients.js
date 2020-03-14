70b300f01b34b192ade66d3c2918ebf680c7bcd9
sagar <sagarkdalavi@gmail.com>
2020-03-14 23:18:03 +0530

    More with useEffect and useCallback

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
 import IngredientForm from './IngredientForm'
 import IngredientList from './IngredientList'
@@ -7,22 +7,22 @@ import Search from './Search'
 const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([])
 
-    useEffect(() => {
-        fetch('https://react-hooks-d3fc1.firebaseio.com/ingredients.json')
-            .then(response => response.json())
-            .then(responseData => {
-                console.log('response data:', responseData)
-                const loadingIngredients = []
-                for (const key in responseData) {
-                    loadingIngredients.push({
-                        id: key,
-                        title: responseData[key].title,
-                        amount: responseData[key].amount,
-                    })
-                }
-                setUserIngredients(loadingIngredients)
-            })
-    }, [])
+    // useEffect(() => {
+    //     fetch('https://react-hooks-d3fc1.firebaseio.com/ingredients.json')
+    //         .then(response => response.json())
+    //         .then(responseData => {
+    //             console.log('response data:', responseData)
+    //             const loadingIngredients = []
+    //             for (const key in responseData) {
+    //                 loadingIngredients.push({
+    //                     id: key,
+    //                     title: responseData[key].title,
+    //                     amount: responseData[key].amount,
+    //                 })
+    //             }
+    //             setUserIngredients(loadingIngredients)
+    //         })
+    // }, [])
 
     const addIngredientHandler = ingredient => {
         fetch('https://react-hooks-d3fc1.firebaseio.com/ingredients.json', {
@@ -45,12 +45,15 @@ const Ingredients = () => {
         )
     }
 
+    const filteredIngredientHandler = useCallback(filteredIngredient => {
+        setUserIngredients(filteredIngredient)
+    }, [])
     return (
         <div className="App">
             <IngredientForm onAddIngredient={addIngredientHandler} />
 
             <section>
-                <Search />
+                <Search onLoadIngredients={filteredIngredientHandler} />
                 <IngredientList
                     ingredients={userIngredients}
                     onRemoveItem={id => {
