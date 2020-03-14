fb0aa1302e8765ee753f5ea14d30d36f73c6efb1
sagar <sagarkdalavi@gmail.com>
2020-03-14 23:36:46 +0530

    Added useRef for avoiding multiple server calls for every key change

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import Card from '../UI/Card'
 import './Search.css'
@@ -6,34 +6,41 @@ import './Search.css'
 const Search = React.memo(props => {
     const { onLoadIngredients } = props
     const [enteredFilter, setEnteredFilter] = useState('')
+    const inputRef = useRef()
     useEffect(() => {
-        const query =
-            enteredFilter.length === 0
-                ? ''
-                : `?orderBy="title"&equalTo="${enteredFilter}"`
-        fetch(
-            'https://react-hooks-d3fc1.firebaseio.com/ingredients.json' + query
-        )
-            .then(response => response.json())
-            .then(responseData => {
-                console.log('response data:', responseData)
-                const loadingIngredients = []
-                for (const key in responseData) {
-                    loadingIngredients.push({
-                        id: key,
-                        title: responseData[key].title,
-                        amount: responseData[key].amount,
+        setTimeout(() => {
+            if (enteredFilter === inputRef.current.value) {
+                const query =
+                    enteredFilter.length === 0
+                        ? ''
+                        : `?orderBy="title"&equalTo="${enteredFilter}"`
+                fetch(
+                    'https://react-hooks-d3fc1.firebaseio.com/ingredients.json' +
+                        query
+                )
+                    .then(response => response.json())
+                    .then(responseData => {
+                        console.log('response data:', responseData)
+                        const loadingIngredients = []
+                        for (const key in responseData) {
+                            loadingIngredients.push({
+                                id: key,
+                                title: responseData[key].title,
+                                amount: responseData[key].amount,
+                            })
+                        }
+                        onLoadIngredients(loadingIngredients)
                     })
-                }
-                onLoadIngredients(loadingIngredients)
-            })
-    }, [enteredFilter, onLoadIngredients])
+            }
+        }, 500)
+    }, [enteredFilter, onLoadIngredients, inputRef])
     return (
         <section className="search">
             <Card>
                 <div className="search-input">
                     <label>Filter by Title</label>
                     <input
+                        ref={inputRef}
                         type="text"
                         value={enteredFilter}
                         onChange={event => {
